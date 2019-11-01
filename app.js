f902efe1131e296eff51f1150bbae5ce252b51e8
Nikhil <nikhil@data.gov.sg>
2019-11-02 01:26:42 +0800

    use env PORT; log root endpoint being hit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello, Elasticbeanstalk!'))
+app.get('/', (req, res) => {
+  console.log("Endpoint hit!")
+  res.send('Hello, Elasticbeanstalk!')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
